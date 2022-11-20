faf0abb11a68e6650d133cd8f92afdc33d778731
Artefius <kostikixum@gmail.com>
2022-11-20 19:53:53 +0700

    otpravka

---
+++ SeminarC#1.01/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ведите число дня недели");
+        int Day = Convert.ToInt32(Console.ReadLine());
+        if (Day == 1) Console.WriteLine("понедельник");
+        if (Day == 2) Console.WriteLine("вторник");
+        if (Day == 3) Console.WriteLine("среда");
+        if (Day == 4) Console.WriteLine("четверг");
+        if (Day == 5) Console.WriteLine("пятница");
+        if (Day == 6) Console.WriteLine("суббота");
+        if (Day == 7) Console.WriteLine("воскресенье");
+        if (Day>7) Console.WriteLine("число введено не верно");
+        if (Day <1) Console.WriteLine("число введено не верно");
+    }
+}
---
+++ SeminarC#1_03/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int div1 = num % 10;
+Console.Write($"{div1} ");
