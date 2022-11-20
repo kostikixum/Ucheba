529eb8f31ab137b5995d5ab4ba06d9349819f1ec
Artefius <kostikixum@gmail.com>
2022-11-20 19:36:57 +0700

    zadacha1

--- Examp003/Program.cs
+++ Examp003/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = new Random().Next(1,10);// случайное число от 1 до 10
+﻿int numberA = new Random().Next(1,10);// случайное число от 1 до 9(max-1)
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
